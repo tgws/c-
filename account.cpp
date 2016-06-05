c94bcde44563b3657b36c7b236bca1bd03aac297
tgws <virus_tt@163.com>
2016-06-05 20:28:30 +0800

    添加savingaccount函数实现

---
+++ account.cpp
@@ -0,0 +1,7 @@
+//savingAccount类相关成员的实现
+//void record(int date,double amount);
+//SavingAccount{int date,int id,double rate};
+//void deposit(int date,double amount);
+//void withdraw(int date,double amount);
+//void settle(int date);
+
